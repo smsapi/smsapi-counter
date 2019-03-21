14db3f5f890d87761beff342c0f88703e8399476
Kewin Polok <k.polok@smsapi.pl>
2019-03-21 07:34:32 +0100

    Basic eslint configuration

--- smsapi-counter.js
+++ smsapi-counter.js
@@ -1,11 +1,12 @@
-var CVSMSCounter = (function () {
-
+var CVSMSCounter = (function() {
     var dataCvSmsTextarea = '[data-cv-sms-textarea]',
         dataCvSmsCounter = '[data-cv-sms-counter]',
         smsTextareasNodeList = document.querySelectorAll(dataCvSmsTextarea);
 
     function isGsmCharacterSet(string) {
-        return /[^A-Z0-9_@£\$¥èéùìòÇØø\+%&\!"#'\(\)\*,\-\.ÅåÆæß¤:;<=>?¡ÄÖÑÜ§¿äöñüà€ΓΔΘΛΞΠΣΦΨΩαβγδεζηθικλμνξοπρστυφχψωςέάόίώύήϊϋΐΰΆΈΊΉΌΎΏXΥΡΟΝΜΚΗΖΕΙΤΑΧΒ\^\{\}\\\[~\]\|\/ \n\r]/ig.test(string);
+        return /[^A-Z0-9_@£\$¥èéùìòÇØø\+%&\!"#'\(\)\*,\-\.ÅåÆæß¤:;<=>?¡ÄÖÑÜ§¿äöñüà€ΓΔΘΛΞΠΣΦΨΩαβγδεζηθικλμνξοπρστυφχψωςέάόίώύήϊϋΐΰΆΈΊΉΌΎΏXΥΡΟΝΜΚΗΖΕΙΤΑΧΒ\^\{\}\\\[~\]\|\/ \n\r]/gi.test(
+            string
+        );
     }
 
     if (smsTextareasNodeList.length) {
@@ -19,7 +20,9 @@ var CVSMSCounter = (function () {
 
     function activate(smsTextarea, smsCounter) {
         if (smsTextarea === undefined || smsCounter === undefined) {
-            throw new Error('CVSMSCounter needs two arguments: textarea, and label characters counter');
+            throw new Error(
+                'CVSMSCounter needs two arguments: textarea, and label characters counter'
+            );
         }
 
         if (typeof smsTextarea === 'string' && typeof smsCounter === 'string') {
@@ -27,17 +30,23 @@ var CVSMSCounter = (function () {
             smsCounter = document.getElementById(smsCounter);
 
             if (smsTextarea === null || smsCounter === null) {
-                throw new Error('CVSMSCounter: passed ID names have no corresponding HTML nodes in current DOM tree.');
+                throw new Error(
+                    'CVSMSCounter: passed ID names have no corresponding HTML nodes in current DOM tree.'
+                );
             }
         }
 
         countCharacters(smsTextarea, smsCounter);
         var eventsArray = ['keyup', 'change', 'paste'];
 
-        eventsArray.forEach(function (event) {
-            smsTextarea.addEventListener(event, function () {
-                countCharacters(this, smsCounter)
-            }, false);
+        eventsArray.forEach(function(event) {
+            smsTextarea.addEventListener(
+                event,
+                function() {
+                    countCharacters(this, smsCounter);
+                },
+                false
+            );
         });
     }
 
@@ -51,39 +60,49 @@ var CVSMSCounter = (function () {
                 symbolsMaxCount: 918,
                 singleSmsLength: 160,
                 secondSmsLength: 147,
-                manySmsLength: 153
-            }
+                manySmsLength: 153,
+            };
         } else {
             smsSettings = {
                 symbolsMaxCount: 402,
                 singleSmsLength: 70,
                 secondSmsLength: 64,
-                manySmsLength: 67
-            }
+                manySmsLength: 67,
+            };
         }
 
         if (textarea.value.length >= smsSettings.symbolsMaxCount) {
-            textarea.value = textarea.value.substring(0, smsSettings.symbolsMaxCount);
+            textarea.value = textarea.value.substring(
+                0,
+                smsSettings.symbolsMaxCount
+            );
         }
 
         var textareaValue = textarea.value,
             textareaLength = textareaValue.length;
 
-        smsPartsCount = textareaLength <= smsSettings.singleSmsLength ? 1 : Math.ceil(textareaLength / smsSettings.manySmsLength);
+        smsPartsCount =
+            textareaLength <= smsSettings.singleSmsLength
+                ? 1
+                : Math.ceil(textareaLength / smsSettings.manySmsLength);
 
         if (smsPartsCount === 1) {
-            charactersLeft = smsSettings.singleSmsLength - (textareaLength);
+            charactersLeft = smsSettings.singleSmsLength - textareaLength;
         } else if (smsPartsCount === 2) {
-            charactersLeft = smsSettings.secondSmsLength - (textareaLength - smsSettings.singleSmsLength);
+            charactersLeft =
+                smsSettings.secondSmsLength -
+                (textareaLength - smsSettings.singleSmsLength);
         } else if (smsPartsCount > 2) {
-            charactersLeft = smsSettings.manySmsLength - (textareaLength - smsSettings.manySmsLength * (smsPartsCount - 1));
+            charactersLeft =
+                smsSettings.manySmsLength -
+                (textareaLength -
+                    smsSettings.manySmsLength * (smsPartsCount - 1));
         }
 
         counter.innerHTML = charactersLeft + '/' + smsPartsCount;
     }
 
     return {
-        activate: activate
+        activate: activate,
     };
-
 })();
