973d2a1ddef0cfd58db444c9f77c283daea1a5c6
Kewin Polok <k.polok@smsapi.pl>
2019-03-21 08:12:48 +0100

    Single sms with gsm characters only calculation test

--- smsapi-counter.spec.js
+++ smsapi-counter.spec.js
@@ -1,19 +1,45 @@
 /* eslint-env es6, jest */
 
+let counter;
+let textarea;
+
+const singleSmsLength = 160;
+
 describe('smsapi-counter', () => {
     beforeEach(() => {
+        jest.resetModules();
+
         document.body.innerHTML =
             '<div>' +
             '  <textarea rows="10" cols="50" data-cv-sms-textarea></textarea>' +
             '  <h2 data-cv-sms-counter></h2>' +
             '</div>';
+
+        counter = document.querySelector('[data-cv-sms-counter]');
+        textarea = document.querySelector('[data-cv-sms-textarea]');
     });
 
     it('should activate when data-cv-sms attributes are specified', () => {
+        // when
         require('./smsapi-counter.js');
 
-        const counterElement = document.querySelector('[data-cv-sms-counter]');
+        // then
+        expect(counter.innerHTML).toEqual('160/1');
+    });
 
-        expect(counterElement.innerHTML).toEqual('160/1');
+    describe('when only gsm characters are used', () => {
+        it('should calculate single sms left characters', () => {
+            // given
+            const someGsmOnlyCharacters = '1234567890abcAbc';
+
+            // when
+            textarea.value = someGsmOnlyCharacters;
+            require('./smsapi-counter.js');
+
+            // then
+            expect(counter.innerHTML).toEqual(
+                `${singleSmsLength - someGsmOnlyCharacters.length}/1`
+            );
+        });
     });
 });
