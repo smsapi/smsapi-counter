7f08cff758bbba66a9dd07ce06568c6a160d3959
Kewin Polok <k.polok@smsapi.pl>
2019-03-21 07:59:57 +0100

    Activation on html attributes test

--- smsapi-counter.spec.js
+++ smsapi-counter.spec.js
@@ -1,5 +1,19 @@
 /* eslint-env es6, jest */
 
 describe('smsapi-counter', () => {
-    it('initial passing test', () => {});
+    beforeEach(() => {
+        document.body.innerHTML =
+            '<div>' +
+            '  <textarea rows="10" cols="50" data-cv-sms-textarea></textarea>' +
+            '  <h2 data-cv-sms-counter></h2>' +
+            '</div>';
+    });
+
+    it('should activate when data-cv-sms attributes are specified', () => {
+        require('./smsapi-counter.js');
+
+        const counterElement = document.querySelector('[data-cv-sms-counter]');
+
+        expect(counterElement.innerHTML).toEqual('160/1');
+    });
 });
